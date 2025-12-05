afc68edb244a87778e1379de074499880824c08d
AlexR4000 <Alexander.Raagas@edu.sait.ca>
2025-12-05 15:03:00 -0700

    added bstreenode implementations

--- src/implementations/BSTreeNode.java
+++ src/implementations/BSTreeNode.java
@@ -2,5 +2,33 @@ package implementations;
 
 public class BSTreeNode<E>
 {
-
+	E data;
+	BSTreeNode<E> left;
+	BSTreeNode<E> right;
+	
+	public BSTreeNode(E data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+	
+	public E getData() {
+		return data;
+	}
+	
+	public BSTreeNode<E> getLeft(){
+		return left;
+	}
+	
+	public BSTreeNode<E> getRight(){
+		return right;
+	}
+	
+	public void setLeft(BSTreeNode<E> left){
+		this.left = left;
+	}
+	
+	public void setRight(BSTreeNode<E> right){
+		this.right = right;
+	}
 }
